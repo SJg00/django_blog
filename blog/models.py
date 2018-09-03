02b0f88bbb4e00e0304f2316b25ff92ae1181458
Steven Grossman <sjgrossman00@gmail.com>
2018-09-03 18:14:25 -0400

    Add feature to create new blog post from front-end.

--- blog/models.py
+++ blog/models.py
@@ -10,11 +10,10 @@ class Post(models.Model):
 
     def publish(self):
         self.published_date = timezone.now()
-        self.save() 
-
+        self.save()
 
     # create a string representation
-    def __str__(self): 
+    def __str__(self):
         return self.title + ' by' + str(self.author)
 
 
--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000 ____> local
+    # mydjangosite.com ___> online
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+
+    # 127.0.0.1:8000/post/2 --> local
+    # mydjangosite.com/post/2 --> online
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+
+    # 127.0.0.1:8000/post/new -->  local
+    # mydjangosite.com/post/new --> online
+    path('post/new', views.post_new, name='post_new'),
 ]
